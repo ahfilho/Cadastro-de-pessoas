a996f7d1f66bdbcb6ad2d70a55a26e322a405da8
dinho <dinho-16@hotmail.com>
2022-09-19 12:11:12 -0300

    Controller, service, repository e identação.

--- src/main/java/br/com/repository/PessoaRepository.java
+++ src/main/java/br/com/repository/PessoaRepository.java
@@ -8,13 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 import br.com.entity.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-//
-//	@Query("select p from Pessoa p where p.cpf like %?1% group by Pessoas")
-//	public Optional<Pessoa> findByCountryCode(String country_code);
 
-
-		@Query("select p from Pessoa p where p.cpf = ?1")
-	public Optional<Pessoa> findByCpf(String cpf);
+    @Query("select p from Pessoa p where p.cpf = ?1")
+    public Optional<Pessoa> findByCpf(String cpf);
 
 
 }
